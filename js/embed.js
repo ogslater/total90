5a2ff02a5a0c4d264093a78fd8ee3b77acf866cc
slater <igor.guterres.lopes.coelho@example.com>
2023-12-03 12:54:44 -0300

    aaa

--- js/embed.js
+++ js/embed.js
@@ -1,8 +1,1 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var e = document.createElement("iframe");
-    e.src = "https://exemplo.com/seu_embed",
-    e.width = "400",
-    e.height = "300",
-    e.frameborder = "0",
-    document.getElementById("embedContainer").appendChild(e)
-});
+document.addEventListener("DOMContentLoaded",function(){var e=document.createElement("iframe");e.src="https://exemplo.com/seu_embed",e.width="400",e.height="300",e.frameborder="0",document.getElementById("embedContainer").appendChild(e)});
